17e9225225eba56ab2c4e7422e6ecc65ce0dcdee
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2020-05-03 17:11:20 -0300

    Creating Auth Redux

---
+++ frontend/src/store/modules/auth/actions.js
@@ -0,0 +1,19 @@
+export function signInRequest(email, password) {
+    return {
+        type: '@auth/SIGN_IN_REQUEST',
+        payload: { email, password },
+    }
+}
+
+export function SignInSuccess(token, user) {
+    return {
+        type: '@auth/SIGN_IN_SUCCESS',
+        PAYLOAD: { token, user },
+    }
+}
+
+export function SignFailure() {
+    return {
+        type: '@auth/SIGN_FAILURE',
+    }
+}
--- frontend/src/store/modules/auth/reducer.js
+++ frontend/src/store/modules/auth/reducer.js
@@ -1,7 +1,18 @@
-const INITTIAL_STATE ={}
+import produce from 'immer';
+
+const INITTIAL_STATE ={
+    toke: null,
+    signed: false,
+    loading: false,
+}
 
 export default function auth(state = INITTIAL_STATE, action){
     switch (action.type) {
+        case '@auth/SIGN_IN_SUCCESS':
+            return produce(state, draft => {
+                draft.token = action.payload.token;
+                draft.signed = true;
+            });
         default:
             return state;
     }
