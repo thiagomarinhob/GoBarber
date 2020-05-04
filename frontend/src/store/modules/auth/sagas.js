4602449c4d16f03cf3899351a8a406cbe6a16cad
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2020-05-04 19:15:42 -0300

    feat: Persist Authorization

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
                 return;
             }
 
+            api.defaults.headers.Authorization = `Bearer ${token}`;
+
             yield put(SignInSuccess(token, user));
 
             history.push('/dashboard');
@@ -32,4 +34,38 @@ export function* signIn({ payload }) {
 
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }){
+    try{
+        const { name, email, password} = payload;
+
+        yield call(api.post, 'users', {
+            name,
+            email,
+            password,
+            provider: true,
+        });
+
+        history.push('/');
+    } catch(err) {
+        toast.error('Falha no cadastro, verifique seus dados!');
+
+        yield put(SignFailure());
+    }
+};
+
+export function setToken({ payload }) {
+    if(!payload) return;
+
+    const { token } = payload.auth;
+
+    if(toke) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
+export default all([
+    takeLatest('persist/REHYDRATE', setToken),
+    takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+    takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+
+]);
